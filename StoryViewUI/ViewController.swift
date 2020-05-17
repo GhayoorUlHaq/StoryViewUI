afc9454e1310058bacd32f4c1e7ff1cc6732a18f
mac <ghayoor10th@gmail.com>
2020-05-17 20:19:05 +0500

    firstCommit

---
+++ StoryViewUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryViewUI
+//
+//  Created by Ghayoor ul Haq on 17/05/2020.
+//  Copyright © 2020 Ghayoor ul Haq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
